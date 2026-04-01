8b5c8392d3f13e7b1bd7f0a09653a135199e822a
kiprotichgidii <gedionkiprotich3@gmail.com>
2026-04-01 13:17:11 +0300

    update deployment script

--- 01-devops-project/cloudformation/deploy.sh
+++ 01-devops-project/cloudformation/deploy.sh
@@ -3,6 +3,5 @@
 aws cloudformation deploy \
   --template-file template.yaml \
   --capabilities CAPABILITY_NAMED_IAM \
-  --no-execute-changeset \
   --no-fail-on-empty-changeset \
   --stack-name devops-static-site-dev
