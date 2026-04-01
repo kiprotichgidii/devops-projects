8c402e497f0a0ea36e3382bc10f8b9438dca7411
kiprotichgidii <gedionkiprotich3@gmail.com>
2026-04-01 12:24:21 +0300

    add cfn deployment script

---
+++ 01-devops-project/cloudformation/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+aws cloudformation deploy \
+  --template-file template.yaml \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --no-execute-changeset \
+  --no-fail-on-empty-changeset \
+  --stack-name devops-static-site-dev
