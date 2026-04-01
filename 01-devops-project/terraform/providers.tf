f4584aa5399277ed547437a624cfa245ebc3064e
kiprotichgidii <gedionkiprotich3@gmail.com>
2026-04-01 12:31:37 +0300

    add state backend

--- 01-devops-project/terraform/providers.tf
+++ 01-devops-project/terraform/providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket         = "devops-static-site-tf-state"
+    key            = "static-site/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "devops-static-site-tf-locks"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
