3b6b9929374f8a3562a0e59a47307ea787bcda67
kiprotichgidii <gedionkiprotich3@gmail.com>
2026-05-06 17:17:23 +0300

    Update architecture diagram.

--- 03-devops-project/diagram.py
+++ 03-devops-project/diagram.py
@@ -5,7 +5,7 @@ from diagrams.onprem.vcs import Github
 from diagrams.onprem.client import User
 from diagrams.generic.os import Ubuntu
 
-with Diagram("Jenkins Server Architecture", show=False, filename="architecture", direction="LR", outformat="png"):
+with Diagram("Jenkins Distributed Architecture", show=False, filename="architecture", direction="LR", outformat="png"):
     user = User("User / Developer")
     admin = User("Admin / Ansible")
     github = Github("GitHub Webhook")
@@ -13,8 +13,10 @@ with Diagram("Jenkins Server Architecture", show=False, filename="architecture",
     with Cluster("AWS Cloud (us-east-1)"):
         with Cluster("Default VPC"):
             with Cluster("Public Subnet (Security Group: ports 8080, 22)"):
-                jenkins = EC2("Jenkins Server\n(t3.small, 20GB gp3 EBS)")
+                jenkins_master = EC2("Jenkins Master\n(t3.small, 20GB gp3 EBS)")
+                jenkins_agent = EC2("Jenkins Agent\n(t3.small, 20GB gp3 EBS)")
                 
-    user >> Edge(label="HTTP (8080)") >> jenkins
-    github >> Edge(label="HTTP (8080)") >> jenkins
-    admin >> Edge(label="SSH (22)") >> jenkins
+    user >> Edge(label="HTTP (8080)") >> jenkins_master
+    github >> Edge(label="HTTP (8080)") >> jenkins_master
+    admin >> Edge(label="SSH (22)") >> [jenkins_master, jenkins_agent]
+    jenkins_master >> Edge(label="SSH (22)") >> jenkins_agent
